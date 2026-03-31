a6a547a973c50a3f7804a1060231b4b81dce8594
guibondezan <gui.gastaldo@gmail.com>
2026-03-31 11:21:04 -0300

    First commit

---
+++ app/Models/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+#[Fillable(['name', 'description', 'password'])]
+#[Hidden(['password', 'remember_token'])]
+
+class User extends Authenticatable
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    // Relacionamento 1:1 com Profile
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    // Relacionamento 1:N com Project
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
+}
